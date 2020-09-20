5819f896fd33fffa663b874e415c65cd973f2dd2
Louis <louispakel@gmail.com>
2020-09-20 22:25:30 +0200

    [1.0.3] HotFix
    ChangeLog :
    - Update animation clip Credit position
    - Add custom key event to load next scene
    - Add custom speed for the animation clip
    - Reorganise code
    - Fix Misssing Script on Content GamObject

--- Editor/credit.cs
+++ Editor/credit.cs
@@ -25,18 +25,47 @@ namespace ArchNet.Module.Credit
 
         [Header("Custom Setup")]
         // Scene to load after the credit
-        public string SceneToLoad;
+        [SerializeField, Tooltip("Next scene to load")]
+        private string _nextScene;
+
         // Name of the text file with all infos
-        public string TextFileName;
+        [SerializeField, Tooltip("Text File to load")]
+        private string _text;
+
+
+
 
         [Header("Custom Images")]
         // Background image of the credit
-        public Sprite BackgroundImg;
-        // Game logo to display
-        public Sprite GameLogoImg;
-        // Entreprise or corporation image to display
-        public Sprite CorporationImg;
+        [SerializeField, Tooltip("BackGround Image")]
+        private Sprite _backgroundImg;
 
+        // Game logo to display
+        [SerializeField, Tooltip("Game Logo")]
+        private Sprite _gameLogoImg;
+
+        // Entreprise or corporation image to display
+        [SerializeField, Tooltip("Corporation / Entreprise logo")]
+        private Sprite _corporationImg;
+
+
+
+        [Header("Animation")]
+        [SerializeField, Tooltip("Time before auto loadScene")]
+        // Current time before loading next scene
+        private int _time;
+
+        [SerializeField, Tooltip("Credit Animation Clip")]
+        // Animation Clip
+        private AnimationClip _animationClip;
+
+        // Animator
+        [SerializeField,Tooltip("Credit Animator")]
+        private Animator _animator;
+
+        // Animator
+        [SerializeField, Tooltip("Animation Speed")]
+        private float _animationSpeed;
 
         #endregion
 
@@ -71,15 +100,15 @@ namespace ArchNet.Module.Credit
         {
             try
             {
-                Console.WriteLine(Constants.CREDIT_LOADSCENE + SceneToLoad);
+                Console.WriteLine(Constants.CREDIT_LOADSCENE + _nextScene);
 
                 // Load async next scene 
-                SceneManager.LoadSceneAsync(SceneToLoad);
+                SceneManager.LoadSceneAsync(_nextScene);
             }
             catch (Exception)
             {
                 // Trigger Error 409
-                Debug.LogError(Constants.ERROR_409 + " (" + SceneToLoad + ").");
+                Debug.LogError(Constants.ERROR_409 + " (" + _nextScene + ").");
             }
         }
 
@@ -88,11 +117,11 @@ namespace ArchNet.Module.Credit
         /// </summary>
         private void ModuleAvailable()
         {
-            if (BackgroundImg == null
-               || GameLogoImg == null
-               || CorporationImg == null
-               || TextFileName == ""
-               || SceneToLoad == "")
+            if (_backgroundImg == null
+               || _gameLogoImg == null
+               || _corporationImg == null
+               || _text == ""
+               || _nextScene == "")
             {
                 // Trigger Error 410
                 Debug.LogError(Constants.ERROR_410);
@@ -119,13 +148,24 @@ namespace ArchNet.Module.Credit
         /// </summary>
         private void Init()
         {
+            _animator.speed = _animationSpeed;
+
+            AnimationEvent lAnimationEvent = new AnimationEvent
+            {
+                functionName = "LoadScene",
+                time = _time
+            };
+
+
+            _animationClip.AddEvent(lAnimationEvent);
+
             // Set all Game object from components
             Text = GameObject.Find(Constants.GB_Text);
             Content = GameObject.Find(Constants.GB_Content);
             Backgounrd = GameObject.Find(Constants.GB_BackGround);
 
             // Get File text ressource
-            CustomText = Resources.Load(TextFileName) as TextAsset;
+            CustomText = Resources.Load(_text) as TextAsset;
         }
 
         /// <summary>
@@ -135,9 +175,12 @@ namespace ArchNet.Module.Credit
         {
             GameObject lCustomImg = Resources.Load(Constants.GB_CustomImg) as GameObject;
 
+            // Load BackGround Image
+            Backgounrd.GetComponent<Image>().sprite = _backgroundImg;
+
             // Instantiate GameLogo
             GameObject lGameLogo = Instantiate(lCustomImg, Content.transform);
-            lGameLogo.GetComponent<Image>().sprite = GameLogoImg;
+            lGameLogo.GetComponent<Image>().sprite = _gameLogoImg;
             lGameLogo.transform.SetSiblingIndex(1);
 
             // Load Text
@@ -146,7 +189,7 @@ namespace ArchNet.Module.Credit
 
             // Instantiate Corporation
             GameObject lCorporation = Instantiate(lCustomImg, Content.transform);
-            lCorporation.GetComponent<Image>().sprite = CorporationImg;
+            lCorporation.GetComponent<Image>().sprite = _corporationImg;
             lCorporation.transform.SetSiblingIndex(3);
         }
 
